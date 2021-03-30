cc7a47ba7d975902f91d348fd79cc083405b4421
emaiiii <airwickmai@gmail.com>
2021-03-30 16:23:23 -0700

    first commit

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Navigation from '../../Components/Navigation/Navigation';
+import './Home.css';
+
+function Home() {
+    return(    
+        <section className="home-page">
+            <Navigation/>
+        </section>
+    );
+}
+
+export default Home;
