0a52ff5c31f415924e54c567875c2e617c192391
emaiiii <airwickmai@gmail.com>
2021-03-31 00:24:12 -0700

    home page in progress

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,20 +1,16 @@
-import React, {Component} from 'react';
-import {Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
+import React from 'react';
+import {Navbar, Nav} from 'react-bootstrap';
 import './Navigation.css';
 
 function Navigation() {
     return(
-        <Navbar bg="light" variant="light">
-            <Navbar.Brand href="#home">FitLib</Navbar.Brand>
+        <Navbar fixed="top">
+            <Navbar.Brand className="navbar-title" href="#home">FitLib</Navbar.Brand>
             <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
+                <Nav.Link className="navbar-link" href="#home">Home</Nav.Link>
+                <Nav.Link className="navbar-link" href="#features">Features</Nav.Link>
+                <Nav.Link className="navbar-link" href="#pricing">Pricing</Nav.Link>
             </Nav>
-            <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-primary">Search</Button>
-            </Form>
         </Navbar>
     );
 }
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
+import {Button} from 'react-bootstrap';
 import Navigation from '../../Components/Navigation/Navigation';
 import './Home.css';
 
 function Home() {
     return(    
-        <section className="home-page">
+        <div className="home-page">
             <Navigation/>
-        </section>
+
+            <section className="banner-container">
+                <h1 className="f1 lh-title banner-text1">Get Exclusive Access to Work Out Content From Your Favorite Fitness Creators</h1>
+                <h1 className="f1 lh-title banner-text2">Become The Best Version of Yourself</h1>
+                
+                <div className="banner-button-container">
+                    <Button className="banner-button" size="lg">Learn More</Button>
+                </div>
+            </section>
+
+
+        </div>
     );
 }
 
