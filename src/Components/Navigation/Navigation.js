4318b199c9ab20e52e6d3cb7ae48f4c8eb10766f
emaiiii <airwickmai@gmail.com>
2021-03-31 22:51:16 -0700

    home page improvements

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -8,7 +8,7 @@ function Navigation() {
             <Navbar.Brand className="navbar-title" href="#home">FitLib</Navbar.Brand>
             <Nav className="mr-auto">
                 <Nav.Link className="navbar-link" href="#home">Home</Nav.Link>
-                <Nav.Link className="navbar-link" href="#features">Features</Nav.Link>
+                <Nav.Link className="navbar-link" href="#features">About Us</Nav.Link>
                 <Nav.Link className="navbar-link" href="#pricing">Pricing</Nav.Link>
             </Nav>
         </Navbar>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Button} from 'react-bootstrap';
 import Navigation from '../../Components/Navigation/Navigation';
+import homeVideo from './landing-video1.mp4';
 import './Home.css';
 
 function Home() {
@@ -8,16 +9,26 @@ function Home() {
         <div className="home-page">
             <Navigation/>
 
-            <section className="banner-container">
-                <h1 className="f1 lh-title banner-text1">Get Exclusive Access to Work Out Content From Your Favorite Fitness Creators</h1>
-                <h1 className="f1 lh-title banner-text2">Become The Best Version of Yourself</h1>
-                
-                <div className="banner-button-container">
-                    <Button className="banner-button" size="lg">Learn More</Button>
+            <div className="hero-container">
+                <section className="banner-container">
+                    <h1 className="f1 lh-title banner-text1">Get Exclusive Access to Work Out Content From Your Favorite Fitness Creators</h1>
+                    <h1 className="f1 lh-title banner-text2">Become The Best Version of Yourself</h1>
+                    
+                    <div className="banner-button-container">
+                        <Button className="banner-button" size="lg">Learn More</Button>
+                    </div>
+                </section>
+
+                <div className="video-content">
+                    <video autoPlay loop muted id='video'>
+                        <source src={homeVideo} type='video/mp4'/>
+                    </video>
                 </div>
-            </section>
+            </div>
 
-
+            <div className="footer">
+                <h1>©FitLib</h1>
+            </div>
         </div>
     );
 }
