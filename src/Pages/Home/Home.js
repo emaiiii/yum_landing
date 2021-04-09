4c6b4e000980fc0437f0ee9d220f59a72040b462
emaiiii <airwickmai@gmail.com>
2021-04-08 17:14:14 -0700

    newsletter component

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -11,8 +11,8 @@ function Home() {
 
             <div className="hero-container">
                 <section className="banner-container">
-                    <h1 className="f1 lh-title banner-text1">Get Exclusive Access to Work Out Content From Your Favorite Fitness Creators</h1>
-                    <h1 className="f1 lh-title banner-text2">Become The Best Version of Yourself</h1>
+                    <h1 className="lh-title font1 banner-text1">Get Exclusive Access to Work Out Content From Your Favorite Fitness Creators</h1>
+                    <h1 className="lh-title font1 banner-text2">Become The Best Version of Yourself</h1>
                     
                     <div className="banner-button-container">
                         <Button className="banner-button" size="lg">Learn More</Button>
@@ -26,8 +26,13 @@ function Home() {
                 </div>
             </div>
 
-            <div className="info1-section">
-                <h1 className="banner-text1 info1-section-text">Learn From the Best to Be the Best</h1>
+            <div className="newsletter-container">
+                <h1 className="font1 newsletter-header">Keep Up With Us While We Are Under Development</h1>
+                <h1 className="newsletter-info">Sign Up For Our Mailing List</h1>
+                <form className="form-container">
+                    <input className="newsletter-input" type="email" id="newsletter-email" placeholder="Your Email Address"></input>
+                    <Button className="newsletter-button" id="newsletter-button" size="lg">Sign Up</Button>
+                </form>
             </div>
         </div>
     );
